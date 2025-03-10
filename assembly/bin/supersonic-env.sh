b533d846868d02270442840a0eca6587f13490a8
yhl9t <foxlee9527@gmail.com>
2025-03-10 22:04:45 +0800

    local env

--- assembly/bin/supersonic-env.sh
+++ assembly/bin/supersonic-env.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 #### Set below DB configs to connect to your own database
-# Supported DB_TYPE:  h2, mysql, postgres
-export S2_DB_TYPE=h2
-export S2_DB_HOST=
-export S2_DB_PORT=
-export S2_DB_USER=
-export S2_DB_PASSWORD=
-export S2_DB_DATABASE=
+# Supported DB_TYPE:  h2, mysql, postgres，Clickhouse
+export S2_DB_TYPE=mysql
+export S2_DB_HOST=localhost
+export S2_DB_PORT=3306
+export S2_DB_USER=root
+export S2_DB_PASSWORD=Root@123
+export S2_DB_DATABASE=supersonic
